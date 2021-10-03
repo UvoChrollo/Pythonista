69a3c520f86eaa21866dafe1af86b067cdd3c3b9
KelvinMulyawan <kelvinmulyawan2000@gmail.com>
2021-10-03 21:21:14 +0700

    New function

---
+++ Python/str_to_date.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+
+def str_to_date(masukkan: str, format: str) -> datetime:
+    """fungsi untuk merubah string menjadi fungsi
+
+    Args:
+        masukkan (str): masukkan input
+        format (str): format datetime
+
+    Returns:
+        datetime: output berupa tanggal
+
+    >>> str_to_date('03/10/2021','%d/%m/%y')
+    03/10/2021
+    """
+    hasil = datetime.strptime(masukkan, format)
+    return hasil
