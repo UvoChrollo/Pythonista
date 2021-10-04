c7d4a3f95bdba2a8e962bb5e086fba2b1e4eaefd
Kelvin Mulyawan <87604509+KelvinMulyawan@users.noreply.github.com>
2021-10-04 21:47:17 +0700

    Add files via upload

---
+++ Python/desc_stat_matrices.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def list_to_matrices(list_of_value: list, shaping: tuple) -> np.ndarray:
+    """merubah list
+
+    Args:
+        list_of_value (list): [description]
+        shaping (tuple): [description]
+
+    Returns:
+        np.ndarray: [description]
+
+    >>> print(list_to_matrices([1,2,3,4],(2,2)))
+    [[1 2]
+     [3 4]]
+    """
+    return np.array(list_of_value.reshape(shaping))
