5ea2fbb88e4da34737e77a33587abdd929067387
KelvinMulyawan <kelvinmulyawan2000@gmail.com>
2021-10-18 20:19:24 +0700

    Face Detection

---
+++ Notebook/Machine Learning/func/stat.py
@@ -0,0 +1,51 @@
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from typing import Optional
+
+
+def stat_hist_desc(
+    column: pd.Series, data: pd.DataFrame, hue: Optional[pd.Series] = None
+):
+
+    """Fungsi untuk menampilkan Histogram beserta informasi tentang Mean, Median, Standard Deviasi
+
+    >>> stat_hist_desc(x=df_mpg['acceleration'],data=df_mpg) # doctest: +SKIP
+    """
+    
+    if column.isnull().sum() > 0:
+        raise ValueError("Maaf, Terdapat null value")
+
+    ax = sns.histplot(x=column, data=data, hue=hue)
+
+    plt.axvline(
+        np.mean(column),
+        color="r",
+        linestyle="--",
+        linewidth=1.5,
+        label="Mean = {:.2f}".format(np.mean(column)),
+    )
+    plt.axvline(
+        np.median(column),
+        color="b",
+        linestyle="--",
+        linewidth=1.5,
+        label="Median = {:.2f}".format(np.median(column)),
+    )
+    plt.axvline(
+        np.std(column),
+        color="y",
+        linestyle="--",
+        linewidth=1.5,
+        label="Std.Dev ={:.2f}".format(np.std(column)),
+    )
+
+    plt.legend(
+        loc="upper center",
+        bbox_to_anchor=(0.5, 1.15),
+        ncol=3,
+        fancybox=True,
+        shadow=True,
+    )
+    plt.show()
---
+++ Notebook/Speciality/Images/requirement/image.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+def show_image(image, title='Image', cmap_type='gray'):
+    plt.imshow(image, cmap=cmap_type)
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
